08e26f76c3fe3de3a7fe28e5dc54a549fc814238
kawahiro311 <hiro.kawasaki0311@gmail.com>
2015-11-19 19:37:50 +0900

    Add hiphenation option to ZipCodeValidator

--- lib/roshi/active_model/validations/zip_code_validator.rb
+++ lib/roshi/active_model/validations/zip_code_validator.rb
@@ -1,8 +1,23 @@
 module ActiveModel
   module Validations
     class ZipCodeValidator < EachValidator
+      REGEXES = {
+        default: /\A\d{3}\-?\d{4}\z/i,
+        with_hiphenation: /\A\d{3}\-\d{4}\z/i,
+        without_hiphenation: /\A\d{7}\z/i
+      }
+
       def validate_each(record, attribute, value)
-        unless value =~ /\A\d{3}\-?\d{4}\z/i
+        regexp = case
+                 when options[:hiphenation].nil?
+                   REGEXES[:default]
+                 when options[:hiphenation]
+                   REGEXES[:with_hiphenation]
+                 when options[:hiphenation] == false
+                   REGEXES[:without_hiphenation]
+                 end
+
+        unless value =~ regexp
           record.errors.add(attribute, options[:message] || I18n.t('errors.messages.invalid'))
         end
       end
--- spec/validations/zip_code_validator_spec.rb
+++ spec/validations/zip_code_validator_spec.rb
@@ -1,32 +1,86 @@
 require 'spec_helper'
 
-class ZipCodeModel < TestModel
+class DefaultZipCodeModel < TestModel
   validates :zip_code, zip_code: true
 end
 
+class WithHiphenationZipCodeModel < TestModel
+  validates :zip_code, zip_code: { hiphenation: true }
+end
+
+class WithOutHiphenationZipCodeModel < TestModel
+  validates :zip_code, zip_code: { hiphenation: false }
+end
+
 describe 'ZipCodeValidator' do
-  context 'valid zip codes' do
-    [
-      '8100001',
-      '810-0001'
-    ].each do |zip_code|
-      it "#{zip_code} should be valid" do
-        expect(ZipCodeModel.new(zip_code: zip_code)).to be_valid
+  describe 'DefaultZipCodeValidation' do
+    context 'valid zip codes' do
+      [
+        '8100001',
+        '810-0001'
+      ].each do |zip_code|
+        it "#{zip_code} should be valid" do
+          expect(DefaultZipCodeModel.new(zip_code: zip_code)).to be_valid
+        end
+      end
+    end
+
+    context 'invalid zip codes' do
+      [
+        '',
+        '00000000',
+        '000-00000',
+        '0000-000',
+        'zipcode',
+        'あいうえお',
+      ].each do |zip_code|
+        it "#{zip_code} should be invalid" do
+          expect(DefaultZipCodeModel.new(zip_code: zip_code)).not_to be_valid
+        end
       end
     end
   end
 
-  context 'invalid zip codes' do
-    [
-      '',
-      '00000000',
-      '000-00000',
-      '0000-000',
-      'zipcode',
-      'あいうえお',
-    ].each do |zip_code|
-      it "#{zip_code} should be invalid" do
-        expect(ZipCodeModel.new(zip_code: zip_code)).not_to be_valid
+  describe 'WithHiphenationZipCodeValidation' do
+    context 'valid zip codes' do
+      [
+        '810-0001'
+      ].each do |zip_code|
+        it "#{zip_code} should be valid" do
+          expect(WithHiphenationZipCodeModel.new(zip_code: zip_code)).to be_valid
+        end
+      end
+    end
+
+    context 'invalid zip codes' do
+      [
+        '8100001'
+      ].each do |zip_code|
+        it "#{zip_code} should be invalid" do
+          expect(WithHiphenationZipCodeModel.new(zip_code: zip_code)).not_to be_valid
+        end
+      end
+    end
+  end
+
+  describe 'WithOutHiphenationZipCodeValidation' do
+    context 'valid zip codes' do
+      [
+        '8100001'
+      ].each do |zip_code|
+        it "#{zip_code} should be valid" do
+          expect(WithOutHiphenationZipCodeModel.new(zip_code: zip_code)).to be_valid
+        end
+      end
+    end
+
+    context 'invalid zip codes' do
+      [
+        '810-0001'
+      ].each do |zip_code|
+        it "#{zip_code} should be invalid" do
+          expect(WithOutHiphenationZipCodeModel.new(zip_code: zip_code)).not_to be_valid
+        end
       end
     end
   end
