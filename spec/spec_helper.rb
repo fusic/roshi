c82776137e22516eb8487ce51010613c0f8a6158
nmbakfm <nmbakfm@gmail.com>
2016-05-17 16:01:55 +0900

    数の大小をフィールドを指定して比較できるバリデータの実装

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,8 @@ class TestModel
   include ActiveModel::Validations
 
   attr_accessor :email, :zip_code, :phone_number,
-                :date, :hiragana, :version_number
+                :date, :hiragana, :version_number,
+                :large_value, :middle_value, :small_value
 
   def initialize(attrs = {})
     attrs.each_pair { |k,v| send("#{k}=", v) }
